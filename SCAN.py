374710da5b58bae5fac5d641e10c0f0f68ada2eb
naturomics <naturomics.liao@gmail.com>
2017-08-21 21:13:22 +0800

    implement scan loss function

--- SCAN.py
+++ SCAN.py
@@ -3,6 +3,16 @@ import tensorflow as tf
 from VAE import Encoder, Decoder
 
 
+# TF 1.3 release the statistical distribution library tf.distributions,
+# support for versions of TF before 1.3
+try:
+    distributions = tf.distributions
+    kl_divergence = tf.distributions.kl_divergence
+except:
+    distributions = tf.contrib.distributions
+    kl_divergence = tf.contrib.distributions.kl_divergence
+
+
 class SCAN(object):
     def __init__(self, cfg):
         self.cfg = cfg
@@ -16,16 +26,45 @@ class SCAN(object):
 
         with tf.variable_scope("beta_VAE"):
             img_q_mu, img_q_sigma = self.img_encoder(img)
-            sym_q_mu, sym_q_sigma = self.sym_encoder(sym)
+            img_z = distributions.Normal(img_q_mu, img_q_sigma)
+            img_gen = self.img_decoder(img_z)
 
-            img_z = tf.exp(img_q_sigma) + img_q_mu
-            sym_z = tf.exp(sym_q_sigma) + sym_q_mu
+            img_reconstruct_error = tf.reduce_mean(img_gen)
 
-            self.img_decoder(img_z)
-            self.sym_decoder(sym_z)
+            img_z_prior = distributions.Normal()
+            KL_divergence = kl_divergence(img_z, img_z_prior)
+            KL_divergence = self.cfg.beta_vae * KL_divergence
+
+            loss = img_reconstruct_error - KL_divergence
+
+        # train beta VAE
+        optimizer = tf.train.AdamOptimizer(self.cfg.learning_rate)
+        train_op = optimizer.minimize(loss)
+
+        for step in range(self.cfg.epoch):
+            self.sess.run(train_op)
 
         with tf.variable_scope("SCAN"):
-            pass
+            sym_q_mu, sym_q_sigma = self.sym_encoder(sym)
+            sym_z = distributions.Normal(sym_q_mu, sym_q_sigma)
+            self.sym_decoder(sym_z)
+
+            sym_reconstruct_error = tf.reduce_mean()
+
+            sym_z_prior = distributions.Normal()
+            beta_KL_divergence = kl_divergence(sym_z, sym_z_prior)
+            beta_KL_divergence = self.cfg.beta_scan * beta_KL_divergence
+            lambda_KL_divergence = kl_divergence(img_z, sym_z)
+
+            loss = sym_reconstruct_error - beta_KL_divergence
+            loss -= self.cfg.lambda_scan * lambda_KL_divergence
+
+        # train SCAN
+        optimizer = tf.train.AdamOptimizer(self.cfg.learning_rate)
+        train_op = optimizer.minimize(loss)
+
+        for step in range(self.cfg.epoch):
+            self.sess.run(train_op)
 
     def inference(self):
         pass
@@ -35,4 +74,6 @@ class SCAN(object):
         Returns:
             data queues of image and symbol.
         """
-        return()
+        img, sym = [], []
+
+        return(img, sym)
