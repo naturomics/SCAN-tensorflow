30d60e5e3f2f9fec260479ba8bf2c32673bbe894
naturomics <naturomics.liao@gmail.com>
2017-08-21 19:03:54 +0800

    Add model init script main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import tensorflow as tf
+from tensorflow import logging
+
+import conf.config
+from SCAN import SCAN
+
+FLAGS = tf.app.flags.FLAGS
+logging.set_verbosity(tf.logging.INFO)
+
+
+def main(_):
+    with tf.Session() as sess:
+        cfg = FLAGS
+        scan = SCAN(sess, cfg)
+        if FLAGS.is_train:
+            logging.info("start training...")
+            scan.train()
+            logging.info("train finished.")
+        else:
+            scan.predict()
+
+
+if __name__ == "__main__":
+    tf.app.run()
