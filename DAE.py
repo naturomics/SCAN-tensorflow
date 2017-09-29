0610a0f44865379a58d626a9cbd4d7596e3cf8f5
naturomics <naturomics.liao@gmail.com>
2017-09-29 17:03:26 +0800

    update DAE model

--- DAE.py
+++ DAE.py
@@ -6,40 +6,40 @@ class Encoder(object):
         self.input = input
         self.scale = scale
 
-    def setup_model(self):
-        self.input = self.input + self.scale * tf.random_normal()
-        self.h1 = tf.contrib.layers.conv2d(self.input, num_outputs=32,
-                                           kernel_size=4, strde=2,
-                                           activation_fn=tf.nn.elu)
-        self.h2 = tf.contrib.layers.conv2d(self.h1, num_outputs=32,
-                                           kernel_size=4, strde=2,
-                                           activation_fn=tf.nn.elu)
-        self.h3 = tf.contrib.layers.conv2d(self.h2, num_outputs=64,
-                                           kernel_size=4, strde=2,
-                                           activation_fn=tf.nn.elu)
-        self.h4 = tf.contrib.layers.conv2d(self.h3, num_outputs=64,
-                                           kernel_size=4, strde=2,
-                                           activation_fn=tf.nn.elu)
+    def __call__(self, input):
+        input = input + self.scale * tf.random_normal()
+        h1 = tf.contrib.layers.conv2d(input, num_outputs=32, kernel_size=4,
+                                      strde=2, activation_fn=tf.nn.elu)
+        h2 = tf.contrib.layers.conv2d(h1, num_outputs=32, kernel_size=4,
+                                      strde=2, activation_fn=tf.nn.elu)
+        h3 = tf.contrib.layers.conv2d(h2, num_outputs=64, kernel_size=4,
+                                      strde=2, activation_fn=tf.nn.elu)
+        h4 = tf.contrib.layers.conv2d(h3, num_outputs=64, kernel_size=4,
+                                      strde=2, activation_fn=tf.nn.elu)
+
+        return(h4)
 
 
 class Decoder(object):
-    def __init__(self, input):
-        self.input = input
+    def __init__(self, cfg):
+        self.cfg = cfg
 
-    def setup_model(self):
-        self.h1 = tf.contrib.layers.conv2d_transpose(self.input,
-                                                     num_outputs=64,
-                                                     kernel_size=4, stride=2,
-                                                     activation_fn=tf.nn.elu)
-        self.h2 = tf.contrib.layers.conv2d_transpose(self.h1, num_outputs=64,
-                                                     kernel_size=4, stride=2,
-                                                     activation_fn=tf.nn.elu)
-        self.h3 = tf.contrib.layers.conv2d_transpose(self.h2, num_outputs=32,
-                                                     kernel_size=4, stride=2,
-                                                     activation_fn=tf.nn.elu)
-        self.h4 = tf.contrib.layers.conv2d_transpose(self.h3, num_outputs=32,
-                                                     kernel_size=4, stride=2,
-                                                     activation_fn=tf.nn.elu)
+    def __call__(self, input):
+        input = tf.contrib.layers.fully_connected(input)
+        h1 = tf.contrib.layers.conv2d_transpose(input, num_outputs=64,
+                                                kernel_size=4, stride=2,
+                                                activation_fn=tf.nn.elu)
+        h2 = tf.contrib.layers.conv2d_transpose(h1, num_outputs=64, stride=2,
+                                                kernel_size=4,
+                                                activation_fn=tf.nn.elu)
+        h3 = tf.contrib.layers.conv2d_transpose(h2, num_outputs=32, stride=2,
+                                                kernel_size=4,
+                                                activation_fn=tf.nn.elu)
+        h4 = tf.contrib.layers.conv2d_transpose(h3, num_outputs=32, stride=2,
+                                                kernel_size=4,
+                                                activation_fn=tf.nn.elu)
+
+        return(h4)
 
 
 if __name__ == "__main__":
